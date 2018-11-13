a34f507e3eba067a595516be86512087583ff000
CHAMI Soufiane <chami.soufiane.fr@gmail.com>
2018-11-13 02:02:12 -0500

    Update Model_fun.R

--- Scripts/Model_fun.R
+++ Scripts/Model_fun.R
@@ -70,12 +70,12 @@ Models_fun=function(i){
   Sensor_data_Agg_ot_MA$Date<-Sensor_data_agg$Dt
   
   
-  #####Traitement des Pannes  ##############
+  ##### Machine Failures Processing  ##############
   #######
   #######
   
   failure_data<-fread(paste(path_data,as.character(Equipment$PanneID[i]),sep=""),sep = ",",header = T)
-  failure_data$Durée<- as.numeric(as.character(gsub(",",".",failure_data$Durée)))
+  failure_data$DurÃ©e<- as.numeric(as.character(gsub(",",".",failure_data$DurÃ©e)))
   
   failure_data$Date<-as.POSIXct(strptime(failure_data$Date, "%d/%m/%Y"))
   failure_data$V1=NULL
@@ -122,7 +122,7 @@ Models_fun=function(i){
   
   IO<- as.data.table(merge(TTF,Sensor_data_agg[,1:4],by="Date"))
   
-####Partition  the data #####################
+#### Data Partition #####################
   
   source(paste(path_scripts,"Metrics.R",sep=""))
   
@@ -144,9 +144,9 @@ Models_fun=function(i){
   
 ###########################################################################################################################
 ###########################################################################################################################
-########################## We shall test at first only the RF model until the interface works well ##########################
-#############################################################################################################################
-#############################################################################################################################
+################ Last updates : 05/25/2017, Models Ensemble still under maintenance untill I finish, the dahsboard ########
+###########################################################################################################################
+###########################################################################################################################
 
   
   
@@ -168,84 +168,84 @@ Models_fun=function(i){
   
   Y=as.data.frame(model_TTF_rf_metrics)
 # 
-# ########Extreme Boosting Gradien #####################
-#   xgb_grid_1 = expand.grid(
-#     nrounds = 100,
-#     eta = c(0.2,0.15,0.1),
-#     max_depth = c(1,6),
-#     gamma = c(2.5,3,3.5),
-#     min_child_weight = c(1,3),
-#     subsample=c(0.5,1),
-#     colsample_bytree = 1
-#   )
-# 
-#   # pack the training control parameters
-#   xgb_trcontrol_1 = trainControl(
-#     method = "cv",
-#     number = 100,
-#     verboseIter = TRUE,
-#     returnData = FALSE,
-#     returnResamp = "all",
-#     allowParallel = TRUE
-#   )
-# 
-#   set.seed(102116)
-#   model_TTF_xgb = train(
-#     x = as.matrix(IO_train[,4:5]),
-#     y = as.numeric(IO_train$TTF_days),
-#     trControl = xgb_trcontrol_1,
-#     tuneGrid = xgb_grid_1,
-#     method = "xgbTree"
-#   )
-# 
-#   Xgb_pred_TTF_pred <- predict(object=model_TTF_xgb,newdata =IO_test[,4:6],type="raw")
-#   Xgb_pred_TTF_train <- predict(object=model_TTF_xgb,newdata =IO_train[,4:6],type="raw")
-# 
-#   plot(Xgb_pred_TTF_pred)
-# 
-#   model_TTF_Xgb_metrics <- evaluate_model(observed = IO_test$TTF_days, predicted = Xgb_pred_TTF_pred)
-#   model_TTF_Xgb_metrics_tr <- evaluate_model(observed = IO_train$TTF_days, predicted = Xgb_pred_TTF_train)
-# 
-# 
+########Extreme Boosting Gradien #####################
+  xgb_grid_1 = expand.grid(
+    nrounds = 100,
+    eta = c(0.2,0.15,0.1),
+    max_depth = c(1,6),
+    gamma = c(2.5,3,3.5),
+    min_child_weight = c(1,3),
+    subsample=c(0.5,1),
+    colsample_bytree = 1
+  )
+
+  # pack the training control parameters
+  xgb_trcontrol_1 = trainControl(
+    method = "cv",
+    number = 100,
+    verboseIter = TRUE,
+    returnData = FALSE,
+    returnResamp = "all",
+    allowParallel = TRUE
+  )
+
+  set.seed(102116)
+  model_TTF_xgb = train(
+    x = as.matrix(IO_train[,4:5]),
+    y = as.numeric(IO_train$TTF_days),
+    trControl = xgb_trcontrol_1,
+    tuneGrid = xgb_grid_1,
+    method = "xgbTree"
+  )
+
+  Xgb_pred_TTF_pred <- predict(object=model_TTF_xgb,newdata =IO_test[,4:6],type="raw")
+  Xgb_pred_TTF_train <- predict(object=model_TTF_xgb,newdata =IO_train[,4:6],type="raw")
+
+  plot(Xgb_pred_TTF_pred)
+
+  model_TTF_Xgb_metrics <- evaluate_model(observed = IO_test$TTF_days, predicted = Xgb_pred_TTF_pred)
+  model_TTF_Xgb_metrics_tr <- evaluate_model(observed = IO_train$TTF_days, predicted = Xgb_pred_TTF_train)
+
+
 # ################GLM##############################################
-# 
-#   model_TTF_GLM=glm(TTF_days ~ .,data=IO_train[,3:5],family = poisson())
-# 
-#   model_TTF_GLM_pred=predict(object = model_TTF_GLM,newdata =IO_test[,4:6],type="response" )
-#   model_TTF_GLM_train=predict(object = model_TTF_GLM,newdata =IO_train[,4:6],type="response" )
-# 
-#   plot(model_TTF_GLM_pred)
-# 
-#   model_TTF_GLM_metrics <- evaluate_model(observed = IO_test$TTF_days, predicted = model_TTF_GLM_pred)
-#   model_TTF_GLM_metrics_tr <- evaluate_model(observed = IO_train$TTF_days, predicted = model_TTF_GLM_train)
-# 
-# 
+
+  model_TTF_GLM=glm(TTF_days ~ .,data=IO_train[,3:5],family = poisson())
+
+  model_TTF_GLM_pred=predict(object = model_TTF_GLM,newdata =IO_test[,4:6],type="response" )
+  model_TTF_GLM_train=predict(object = model_TTF_GLM,newdata =IO_train[,4:6],type="response" )
+
+  plot(model_TTF_GLM_pred)
+
+  model_TTF_GLM_metrics <- evaluate_model(observed = IO_test$TTF_days, predicted = model_TTF_GLM_pred)
+  model_TTF_GLM_metrics_tr <- evaluate_model(observed = IO_train$TTF_days, predicted = model_TTF_GLM_train)
+
+
 # #####################GBM###########################################
-# 
-#   # train the model for each parameter combination in the grid,
-#   #   using CV to evaluate
-#   set.seed(032017)
-# 
-#   model_TTF <- gbm(formula = TTF_days ~ .,
-#                    distribution=  "gaussian",
-#                    data = IO_train[,3:5],
-#                    n.trees = 70,
-#                    interaction.depth = 5,
-#                    shrinkage = 0.3,
-#                    bag.fraction = 0.5,
-#                    train.fraction = 1.0,
-#                    n.cores = NULL)
-# 
-#   model_TTF_GBM_pred=predict (object = model_TTF,newdata =IO_test[,4:5], n.trees =70,type="response")
-#   model_TTF_GBM_train=predict (object = model_TTF,newdata =IO_train[,4:5], n.trees =70,type="response")
-# 
-# 
-# 
-#   plot(model_TTF_GBM_pred)
-# 
-#   model_TTF_GBM_metrics <- evaluate_model(observed = IO_test$TTF_days, predicted = model_TTF_GBM_pred)
-#   model_TTF_GBM_metrics_tr <- evaluate_model(observed = IO_train$TTF_days, predicted = model_TTF_GBM_train)
-# 
+
+  # train the model for each parameter combination in the grid,
+  #   using CV to evaluate
+  set.seed(032017)
+
+  model_TTF <- gbm(formula = TTF_days ~ .,
+                   distribution=  "gaussian",
+                   data = IO_train[,3:5],
+                   n.trees = 70,
+                   interaction.depth = 5,
+                   shrinkage = 0.3,
+                   bag.fraction = 0.5,
+                   train.fraction = 1.0,
+                   n.cores = NULL)
+
+  model_TTF_GBM_pred=predict (object = model_TTF,newdata =IO_test[,4:5], n.trees =70,type="response")
+  model_TTF_GBM_train=predict (object = model_TTF,newdata =IO_train[,4:5], n.trees =70,type="response")
+
+
+
+  plot(model_TTF_GBM_pred)
+
+  model_TTF_GBM_metrics <- evaluate_model(observed = IO_test$TTF_days, predicted = model_TTF_GBM_pred)
+  model_TTF_GBM_metrics_tr <- evaluate_model(observed = IO_train$TTF_days, predicted = model_TTF_GBM_train)
+
 # ########### Neural Network##################################
 # 
 # 
@@ -256,43 +256,43 @@ Models_fun=function(i){
 # 
 # 
 # #############Metrics for ALL Models ####################################
-# 
-#   metrics_df <- rbind(model_TTF_GBM_metrics, model_TTF_GLM_metrics, model_TTF_Xgb_metrics,model_TTF_rf_metrics)
-#   rownames(metrics_df) <- NULL
-#   Algorithms <- c("Genralized Boosting Model",
-#                   "Poisson Regression Model",
-#                   "Extreme Gradient Boosting",
-#                   "Random Forest")
-#   metrics_df <- cbind(Algorithms, metrics_df)
-# 
-#   metrics_df_train <- rbind(model_TTF_GBM_metrics_tr, model_TTF_GLM_metrics_tr, model_TTF_Xgb_metrics_tr,model_TTF_rf_metrics_tr)
-#   rownames(metrics_df_train) <- NULL
-# 
-#   metrics_df_train <- cbind(Algorithms, metrics_df_train)
-#   setwd(paste(path_scripts,"/Results/",sep=""))
-#   num.file <- paste(getwd(), "/Metrics_Test.csv", sep= "")
-#   write.csv(metrics_df,num.file)
-#   num.file <- paste(getwd(), "/Metrics_Train.csv", sep= "")
-#   write.csv(metrics_df_train,num.file)
-# 
-# 
-# 
-#   matplot (x=IO_train$Date, cbind (IO_train$TTF_days,Xgb_pred_TTF_train,model_TTF_rf_train),
-#            main = "Predicting Time To Failure  (Training Sample)",xlab = "Date",ylab = "Time To Failure", pch = 19)
-# 
-#   axis.Date(1, at=seq(IO_train$Date[1],IO_train$Date[nrow(IO_train)],"month"), format="%m/%Y")
-#   legend("topright", legend = c("Real","Extreme Gradient Boosting (R-square=99.34%)","Random Forest (R-square=93%)"),col =c("black","red","green"),pch=19)
-# 
-# 
-# 
-# 
-#   matplot (x=IO_test$Date, cbind (IO_test$TTF_days,Xgb_pred_TTF_pred,model_TTF_rf_pred),
-#            main = "Predicting Time To Failure  (Testing Sample)",xlab = "Date",ylab = "Time To Failure", pch = 19)
-# 
-#   axis.Date(1, at=seq(IO_test$Date[1],IO_test$Date[nrow(IO_test)],"month"), format="%m/%Y")
-#   legend("topright", legend = c("Real","Extreme Gradient Boosting (R-square=58.30%)","Random Forest(R-square=63.4%)"),col =c("black","red","green"),pch=19)
-# 
-# 
+
+  metrics_df <- rbind(model_TTF_GBM_metrics, model_TTF_GLM_metrics, model_TTF_Xgb_metrics,model_TTF_rf_metrics)
+  rownames(metrics_df) <- NULL
+  Algorithms <- c("Genralized Boosting Model",
+                  "Poisson Regression Model",
+                  "Extreme Gradient Boosting",
+                  "Random Forest")
+  metrics_df <- cbind(Algorithms, metrics_df)
+
+  metrics_df_train <- rbind(model_TTF_GBM_metrics_tr, model_TTF_GLM_metrics_tr, model_TTF_Xgb_metrics_tr,model_TTF_rf_metrics_tr)
+  rownames(metrics_df_train) <- NULL
+
+  metrics_df_train <- cbind(Algorithms, metrics_df_train)
+  setwd(paste(path_scripts,"/Results/",sep=""))
+  num.file <- paste(getwd(), "/Metrics_Test.csv", sep= "")
+  write.csv(metrics_df,num.file)
+  num.file <- paste(getwd(), "/Metrics_Train.csv", sep= "")
+  write.csv(metrics_df_train,num.file)
+
+
+
+  matplot (x=IO_train$Date, cbind (IO_train$TTF_days,Xgb_pred_TTF_train,model_TTF_rf_train),
+           main = "Predicting Time To Failure  (Training Sample)",xlab = "Date",ylab = "Time To Failure", pch = 19)
+
+  axis.Date(1, at=seq(IO_train$Date[1],IO_train$Date[nrow(IO_train)],"month"), format="%m/%Y")
+  legend("topright", legend = c("Real","Extreme Gradient Boosting (R-square=99.34%)","Random Forest (R-square=93%)"),col =c("black","red","green"),pch=19)
+
+
+
+
+  matplot (x=IO_test$Date, cbind (IO_test$TTF_days,Xgb_pred_TTF_pred,model_TTF_rf_pred),
+           main = "Predicting Time To Failure  (Testing Sample)",xlab = "Date",ylab = "Time To Failure", pch = 19)
+
+  axis.Date(1, at=seq(IO_test$Date[1],IO_test$Date[nrow(IO_test)],"month"), format="%m/%Y")
+  legend("topright", legend = c("Real","Extreme Gradient Boosting (R-square=58.30%)","Random Forest(R-square=63.4%)"),col =c("black","red","green"),pch=19)
+
+
 # 
 #   
 #   
